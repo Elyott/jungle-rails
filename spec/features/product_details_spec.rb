26f9ac199449a8f894e853727550aeff9532d754
Elyott Ryan <elyott_ryan@hotmail.com>
2018-05-01 20:06:01 +0000

    added test to navigate to product page from home page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates to the Product Details page", type: :feature, js: true do
 
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,15 +16,16 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-
-
-  scenario "They see all products" do
+  scenario "They see the details for the single product" do
 
     visit root_path
 
+    first('article.product').find('img').click
+
+    # sleep 2
+
     # save_screenshot
 
-    expect(page).to have_css 'article.product', count: 10
   end
 
 end
